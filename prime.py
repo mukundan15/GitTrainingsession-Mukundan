74cdb212d6bf92bb03695a0bd7329b53e18e084e
Mukundan Mahalingam <mukundan.mahalingam@gmail.com>
2025-08-19 22:30:54 +0530

    feat(prime): add function to check prime numbers

---
+++ prime.py
@@ -0,0 +1,21 @@
+flag = False
+num = 2
+
+#num = int(input("enter a number "))  --> u can also try this by asking input from user
+
+if num <= 1:
+	print("It is not a prime number")
+
+elif num > 1:
+	for i in range(2, num):
+		if (num % i) == 0:
+			flag = True
+			break
+	if not flag:
+		print(num, "is a prime number")
+
+	else:
+		print(num, "is not a prime number")
+
+
+
